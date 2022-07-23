f923a9afb461fe7578691848616acd0ecc754768
unknown <hetarth002@gmail.com>
2022-07-23 15:02:48 +0530

    add navbar

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navbar";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
+}
+
+export default App;
