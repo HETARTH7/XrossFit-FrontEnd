09bab281b22345630b41aa9b0d8e1b255a60bc8e
unknown <hetarth002@gmail.com>
2022-07-23 21:59:41 +0530

    route bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Navbar";
+import Test from "./Test";
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </Router>
   );
 }
