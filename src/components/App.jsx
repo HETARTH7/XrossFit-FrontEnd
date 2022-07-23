413e849fea98c89442870fda6f7e531141a29755
unknown <hetarth002@gmail.com>
2022-07-23 22:09:57 +0530

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./Navbar";
-import Test from "./Test";
+import ExercisesList from "./ExercisesList";
+import EditExercise from "./EditExercise";
+import CreateExercise from "./CreateExercise";
+import CreateUser from "./CreateUser";
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<ExercisesList />} />
+        <Route path="/edit/:id" element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
       </Routes>
     </Router>
   );
--- src/components/Test.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Test() {
-  return <h1>Hello Asshole</h1>;
-}
-export default Test;
