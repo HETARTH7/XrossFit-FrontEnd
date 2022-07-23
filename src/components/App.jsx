c5f6ad5eb046902efd0206dc7e6bdbd174854e90
unknown <hetarth002@gmail.com>
2022-07-23 22:14:33 +0530

    routes complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,18 @@ import CreateExercise from "./CreateExercise";
 import CreateUser from "./CreateUser";
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<ExercisesList />} />
-        <Route path="/edit/:id" element={<EditExercise />} />
-        <Route path="/create" element={<CreateExercise />} />
-        <Route path="/user" element={<CreateUser />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <Router>
+        <Navbar />
+        <br />
+        <Routes>
+          <Route path="/" element={<ExercisesList />} />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
