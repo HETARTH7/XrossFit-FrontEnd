290da2a056daf1a2de3ed0c22bc7369a4da2d111
unknown <hetarth002@gmail.com>
2022-09-16 22:07:28 +0530

    install chakra ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import About from "./About";
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 };
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const newUserEnter = (e) => {
-    setUsername(e.target.value);
-  };
-  const newPasswordEnter = (e) => {
-    setPassword(e.target.value);
-  };
-  const onClick = (e) => {
-    e.preventDefault();
-    const newUser = {
-      username,
-      password,
-    };
-    axios
-      .post("http://localhost:5000/user/auth", newUser)
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
-  };
   return (
     <div>
       <form>
         <label>Email</label>
-        <input onChange={newUserEnter} />
+        <input />
         <label>Password</label>
-        <input onChange={newPasswordEnter} />
-        <button onClick={onClick}>Submit</button>
+        <input />
+        <button>
+          <Link to={"/about"}>Sign In</Link>
+        </button>
       </form>
     </div>
   );
