56fe5bfc9b430477ccae3c09e919b8538a4cab34
unknown <hetarth002@gmail.com>
2022-09-18 23:22:34 +0530

    add about drower

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,77 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Heading,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Text,
+} from "@chakra-ui/react";
 import Typewriter from "typewriter-effect";
 import HomeNav from "./HomeNav";
 import Footer from "./Footer";
 
 const Home = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
   return (
     <div>
       <HomeNav />
-      <Heading color="#fff" fontSize={60}>
+      <Heading color="#fff" fontSize={100}>
         <Typewriter
           onInit={(typewriter) => {
             typewriter
               .typeString("HELLO")
               .pauseFor(800)
               .deleteAll()
-              .typeString("Welcome to the GYM App")
+              .typeString(
+                '<strong>Welcome to the<span style="color: #D2001A;"> GYM</span> App</strong>'
+              )
               .start();
           }}
         />
       </Heading>
+      <>
+        <Button mt={20} ref={btnRef} colorScheme="red" onClick={onOpen}>
+          About
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerHeader>About</DrawerHeader>
 
+            <DrawerBody>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Text>
+            </DrawerBody>
+
+            <DrawerFooter>
+              <Button variant="outline" mr={3} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="blue">Learn More</Button>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
       <Footer />
     </div>
   );
--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -7,11 +7,11 @@ const HomeNav = () => {
     <div>
       <Box pt={5} pb={100} pr={5} textAlign={"right"}>
         <Button colorScheme="#000" color="#eee">
-          <Link to={"/login"} style={{ textDecoration: "none"}}>
+          <Link to={"/login"} style={{ textDecoration: "none" }}>
             Login
           </Link>
         </Button>
-        <Button colorScheme="#000" color="#eee">
+        <Button colorScheme="#000" color="#D2001A">
           <Link to={"/register"} style={{ textDecoration: "none" }}>
             Register
           </Link>
