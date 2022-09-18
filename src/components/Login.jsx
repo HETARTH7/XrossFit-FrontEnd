88d357688c38cf5791513c2859b551c7d3666933
unknown <hetarth002@gmail.com>
2022-09-18 22:37:42 +0530

    login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Link,
+  Box,
+} from "@chakra-ui/react";
+import Footer from "./Footer";
 const Login = () => {
   return (
     <div>
-      <form>
-        <label>Email</label>
-        <input />
-        <label>Password</label>
-        <input />
-        <button>
-          <Link to={"/about"}>Sign In</Link>
-        </button>
-      </form>
+      <Box
+        w={[200, 300, 400]}
+        h={[300, 400, 500]}
+        mt={[50, 100, 150]}
+        ml={[50, 110, 650]}
+        borderRadius={[20]}
+        backgroundColor="#eee"
+      >
+        <FormControl>
+          <FormLabel textAlign={"center"} fontSize={30}>
+            Login
+          </FormLabel>
+          <Input mt={30} type="email" placeholder="Enter your email" />
+          <Input
+            mt={30}
+            mb={10}
+            type="password"
+            placeholder="Enter your password"
+          />
+          <Button colorScheme="red" ml={160} mt={20}>
+            <Link style={{ textDecoration: "none" }}>Login</Link>
+          </Button>
+        </FormControl>
+      </Box>
+
+      <Footer />
     </div>
   );
 };
