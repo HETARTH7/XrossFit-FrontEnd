bf64852e65599fab1cfe6088f60815b155948c5d
unknown <hetarth002@gmail.com>
2022-09-29 22:04:02 +0530

    add exercise

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,126 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import Navbar from "./Navbar";
 
-const Dashboard = () => {
-  // Create a clock in dashboard. Auto-Refeshing to display real time every second/
+const CreateExercise = () => {
+  const [username, setUsername] = useState("");
+  const [exerciseName, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
+  };
+  const onChangeDate = (date) => {
+    setDate(date);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const exercise = {
+      username: username,
+      exerciseName: exerciseName,
+      duration: duration,
+      date: date,
+    };
+    console.log(exercise);
+    axios
+      .post("http://localhost:5000/exercise/add", exercise)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/user")
+      .then((res) => {
+        if (res.data.length > 0) {
+          setUsers(res.data.map((user) => user.username));
+          setUsername(res.data[0].username);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div>
       <Navbar />
+      <h3>Add Exercises</h3>
+      <br />
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>
+            <h5>USERNAME</h5>
+          </label>
+          <br />
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}
+          >
+            {users.map((user) => {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+          <br />
+        </div>
+        <div className="form-group">
+          <label>
+            <h5>EXERCISE</h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={exerciseName}
+            onChange={onChangeDescription}
+          />
+          <br />
+        </div>
+        <div className="form-group">
+          <label>
+            <h5>DURATION</h5>
+          </label>
+          <br />
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={duration}
+            onChange={onChangeDuration}
+          />
+          <br />
+        </div>
+        <div className="form-group">
+          <label>
+            <h5>DATE</h5>
+          </label>
+          <div>
+            <DatePicker selected={date} onChange={onChangeDate} />
+          </div>
+        </div>
+        <div className="form-group">
+          <br />
+          <input type="submit" value="ADD" className="btn btn-primary" />
+          <br />
+        </div>
+      </form>
     </div>
   );
 };
 
-export default Dashboard;
+export default CreateExercise;
