ba065f549d3943a33f7bce368cc505d21968f6d8
unknown <hetarth002@gmail.com>
2022-12-02 17:13:39 +0530

    auth and sessions

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import Back from "./Back";
+import axios from "axios";
 
 const Login = () => {
-  const [user, setUser] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [auth, setAuth] = useState("");
+  const [mssg, setMssg] = useState("");
   const handleUser = (e) => {
-    setUser(e.target.value);
+    setUsername(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
   };
 
   const login = (e) => {
     e.preventDefault();
-    sessionStorage.setItem("user", user);
-    window.location = "/dashboard";
+    const user = { username, password };
+    console.log(user);
+    axios
+      .post("http://localhost:5000/auth", user)
+      .then((res) => setAuth(res.data.message))
+      .catch((err) => console.log(err));
   };
+  useEffect(() => {
+    if (auth === "OK") {
+      sessionStorage.setItem("user", username);
+      window.location = "/dashboard";
+    } else if (auth === "Wrong") {
+      setMssg("Invalid. Please try again");
+    }
+  }, [username, auth]);
   return (
     <div>
       <Back />
@@ -29,7 +48,9 @@ const Login = () => {
             className="form-input"
             type="password"
             placeholder="Enter your password"
+            onChange={handlePassword}
           />
+          <p id="mssg">{mssg}</p>
           <button className="form-button">Login</button>
         </form>
       </div>
